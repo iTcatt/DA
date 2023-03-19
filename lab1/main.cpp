9720b2cb1b6b7fe61165542845304c56d9203599
Iurii Kataev <iurii.kataev@gmail.com>
2023-03-19 23:06:54 +0300

    Сделал рефакторинг

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,58 +3,78 @@
 #include <string.h>
 #include <math.h>
 
-struct Node {
+struct Data {
     unsigned long long key;
-    char* value;
+    char* string;
 };
 
-void CountingSort(Node* array, int size, int radix) {
+void CountingSort(Data* data_array, int array_size, int discharge) {
     int counter[10] = {0};
-    unsigned long long r = std::pow(10, radix);
+    unsigned long long ten_power_discharge = std::pow(10, discharge);
     
-    for (int i = 0; i < size; ++i) {
-        counter[(array[i].key / r) % 10] += 1;
+    for (int i = 0; i < array_size; ++i) {
+        counter[(data_array[i].key / ten_power_discharge) % 10] += 1;
     }
     for (int i = 1; i < 10; ++i) {
         counter[i] += counter[i-1];
     }
 
-    Node* helper = new Node[size];
-    for (int i = 0; i < size; ++i) {
-        helper[i] = array[i];
+    Data* copy_input_array = new Data[array_size];
+    for (int i = 0; i < array_size; ++i) {
+        copy_input_array[i] = data_array[i];
     }
    
-    for (int i = size - 1; i >= 0; --i) {
-        array[counter[helper[i].key / r % 10] - 1] = helper[i];
-        counter[helper[i].key / r % 10] -= 1;
+    for (int i = array_size - 1; i >= 0; --i) {
+        int discharge_digit = copy_input_array[i].key / ten_power_discharge % 10;
+        data_array[counter[discharge_digit] - 1] = copy_input_array[i];
+        counter[discharge_digit] -= 1;
     }
-    delete[] helper;
+    delete[] copy_input_array;
 }
 
-void RadixSort(Node* array, int size, int max_radix) {
-    for (int i = 0; i < max_radix; ++i) {
-        CountingSort(array, size, i);
+void RadixSort(Data* data_array, int array_size, int max_discharge) {
+    for (int discharge = 0; discharge < max_discharge; ++discharge) {
+        CountingSort(data_array, array_size, discharge);
     }
 }
 
-int FindMaxDischarge(Node* array, int size) {
-    int max_radix = -1;
-    for (int i = 0; i < size; ++i) {
-        unsigned long long number = array[i].key;
+int FindMaxDischarge(Data* data_array, int array_size) {
+    int max_discharge = -1;
+    for (int i = 0; i < array_size; ++i) {
+        unsigned long long number = data_array[i].key;
         int current = 0; 
         while (number > 0) {
             ++current;
             number /= 10;
         }
-        max_radix = std::max(current, max_radix);
+        max_discharge = std::max(current, max_discharge);
     }
-    return max_radix;
+    return max_discharge;
+}
+
+Data* AddMemory(Data* data_array, int &array_capacity) {
+    Data* tmp = data_array;
+    data_array = new Data[array_capacity * 2];
+    for (int i = 0; i < array_capacity; ++i) {
+        data_array[i] = tmp[i];
+    }
+    array_capacity *= 2;
+    delete[] tmp;
+    return data_array;
+}
+
+void PrintAndDeleteArray(Data* data_array, int array_size) {
+    for (int i = 0; i < array_size; ++i) {
+        printf("%llu\t%s\n", data_array[i].key, data_array[i].string);
+        delete[] data_array[i].string;
+    }   
+    delete[] data_array;
 }
 
 int main() {
-    int size = 0;
-    int capacity = 1;
-    Node* array = new Node[capacity];
+    int array_size = 0;
+    int array_capacity = 1;
+    Data* data_array = new Data[array_capacity];
     FILE* input = fopen("input.txt", "r");
     unsigned long long key;  
     char buffer[2048];
@@ -64,25 +84,15 @@ int main() {
             str[i] = buffer[i];
         }
         str[strlen(buffer)] = '\0';
-        if (size == capacity) {
-            Node* tmp = array;
-            array = new Node[capacity * 2];
-            for (int i = 0; i < capacity; ++i) {
-                array[i] = tmp[i];
-            }
-            capacity *= 2;
-            delete[] tmp;
+        if (array_size == array_capacity) {
+            data_array = AddMemory(data_array, array_capacity);
         }
-        array[size] = {key, str};
-        ++size;
+        data_array[array_size] = {key, str};
+        ++array_size;
     }
-    int max_radix = FindMaxDischarge(array, size);
-    RadixSort(array, size, max_radix);
-    for (int i = 0; i < size; ++i) {
-        printf("%llu\t%s\n", array[i].key, array[i].value);
-        delete[] array[i].value;
-    }
-    
-    delete[] array;
+    fclose(input);
+    int max_discharge = FindMaxDischarge(data_array, array_size);
+    RadixSort(data_array, array_size, max_discharge);
+    PrintAndDeleteArray(data_array, array_size);
     return 0;
 }
